350017383d457439172354a9ac6f5ab90f6b0fb5
weareandrei <weareandrei@gmail.com>
2023-05-28 15:11:32 +0300

    Rollback some DungeonMap bad changes. Now code is working.

--- Assets/Scripts/Dungeon/Model/Base/DungeonMap.cs
+++ Assets/Scripts/Dungeon/Model/Base/DungeonMap.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 namespace Dungeon.Model {
     public abstract class DungeonMap {
         public Grid2DResizable map;
+        
+        public DungeonMap(int sizeX, int sizeY) {
+            map = new Grid2DResizable(sizeX, sizeY);
+        }
+
+        public DungeonMap (int size) {
+            map = new Grid2DResizable(size);
+        }
+
+        public DungeonMap ()  {
+            map = new Grid2DResizable(Consts.ChunkSize);
+        }
 
         public void PlaceCellOnMap(Vector2Int coordinates, string contents) {
             map.UpdateCell(coordinates.x, coordinates.y, contents);
