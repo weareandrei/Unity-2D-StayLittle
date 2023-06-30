b34a557d6f642638da704842c201d26d106ca214
weareandrei <weareandrei@gmail.com>
2023-06-30 18:47:45 +0300

    Improved the Collectible Spawning Architecture

---
+++ Assets/Scripts/Content/Base/ContentSpawner.cs
@@ -0,0 +1,5 @@
+namespace Content {
+    public static class ContentSpawner {
+        
+    }
+}
--- Assets/Scripts/Content/Model/ContentPoint.cs
+++ Assets/Scripts/Content/Model/ContentPoint.cs
@@ -6,10 +6,8 @@ namespace Content {
         [SerializeField]
         public ContentType type;
         public ContentPayload payload;
-
-        // public ContentPointData GetContentPointData() {
-        //     return data;
-        // }
-        
+        // public ContentSpawner spawner;
+        // todo: I've decided to use this as a static class and pass
+        // todo:   all the required data to it every time
     }
 }
